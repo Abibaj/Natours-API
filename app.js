93cbc71eb6cfb23a7267e2854322436ad2f38dad
abibaj <abiolaibraheemajibola@gmail.com>
2022-10-12 10:05:23 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+///// THIRD-PARTY MODULES
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+///// OUR OWN MODULES
+// ERROR HANDLER MODULES
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
+// ROUTERS
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+//
+const app = express();
+
+///// MIDDLEWARES
+app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') app.use(express.json());
+app.use(bodyParser.json());
+
+app.use(express.static(`${__dirname}/public/overview.html`));
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
---
+++ models/tourModel.js
@@ -0,0 +1,125 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+// const validator = require('validator');
+
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+      maxLength: [40, 'A tour can not have more than 40 characters'],
+      minLength: [10, 'A tour can not have less than 10 characters'],
+    },
+    slug: String,
+    duration: { type: Number, required: [true, 'A tour must have a duration'] },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty can only be either: easy, medium or difficulty',
+      },
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be at least 1.0'],
+      max: [1, 'Rating can not be over 5.0 '],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: {
+      type: Number,
+      // validate: {
+      //   validator: function (val) {
+      //     // This only points to current doc on NEW document creation
+      //     return val < this.price;
+      //   },
+      //   message: 'price discount ({VALUE}) cannot be greater than the price',
+      // },
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have cover image'],
+    },
+    images: {
+      type: [String],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
+  },
+  {
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
+});
+
+///// DOCUMENT MIDDLEWARE: Runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save document...');
+//   next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//   next();
+// });
+
+///// QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  next();
+});
+
+tourSchema.post(/^post/, function (docs, next) {
+  // console.log(docs);
+  next();
+});
+
+///// AGGREGATE MIDDLEWARE
+tourSchema.pre(/aggregate/, function (next) {
+  this.pipeline().unshift({ match: { secretTour: { $ne: true } } });
+  next();
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
