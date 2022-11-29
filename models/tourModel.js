11f74d5000059f758780932be97f784bc25ef75e
abibaj <abiolaibraheemajibola@gmail.com>
2022-11-29 19:57:18 +0100

    Learned populating a reference document

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -97,6 +98,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
   },
   {
     secretTour: {
@@ -120,27 +122,28 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-// tourSchema.pre('save', function (next) {
-//   console.log('Will save document...');
-//   next();
-// });
-
-// tourSchema.post('save', function (doc, next) {
-//   next();
-// });
-
 ///// QUERY MIDDLEWARE
 tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
   next();
 });
 
-tourSchema.post(/^post/, function (docs, next) {
-  // console.log(docs);
+tourSchema.pre(/^find/, function (next) {
+  this.populate({ path: 'guides', select: '-__v' });
   next();
 });
 
-///// AGGREGATE MIDDLEWARE
+// tourSchema.pre(/^find/, async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
+
+tourSchema.post(/^post/, function (docs, next) {
+  next();
+});
+
+///// AGGREGATION MIDDLEWARE
 tourSchema.pre(/aggregate/, function (next) {
   this.pipeline().unshift({ match: { secretTour: { $ne: true } } });
   next();
