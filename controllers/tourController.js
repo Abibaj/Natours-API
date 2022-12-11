4fc07c1d7127b1cd0ffcb6f73a12dbd5e495b86a
abibaj <abiolaibraheemajibola@gmail.com>
2022-12-11 21:51:08 +0100

    Good to be back

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,6 +2,7 @@ const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -68,16 +69,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     data: { tour },
   });
 });
-///// HANDLING A DELETE REQUEST /////
-exports.deleteTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndDelete(req.params.id);
-  if (!tour) return next(new AppError('No tour found with that ID', 404));
 
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-});
+///// HANDLING A DELETE REQUEST /////
+exports.deleteTour = factory.deleteOne(Tour);
+// });
+
 exports.getTourStats = catchAsync(async (req, res, next) => {
   const stats = await Tour.aggregate([
     { $match: { ratingsAverage: { $gte: 4.5 } } },
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -25,6 +25,7 @@ router
   .route('/')
   .get(userController.getAllUsers)
   .post(userController.createUser);
+
 router
   .route('/:id')
   .get(userController.getUser)
